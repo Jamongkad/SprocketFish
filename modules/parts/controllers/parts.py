84679ef53562a9036d85de6dab8ff3df8e7fbaba
Mathew <mathew@mathew-laptop.(none)>
2011-01-11 00:45:06 +0800

    d

--- modules/parts/controllers/parts.py
+++ modules/parts/controllers/parts.py
@@ -47,8 +47,10 @@ class search(object):
         
         ids_list = None
 
+        search_set_redis = "-".join(search_query.split(" "))
+
         if ids: 
-            if r_server.get("search_results:%s" % "-".join(search_query.split(" "))):
+            if r_server.get("search_results:%s" % search_set_redis):
                 print "from cache:search_results"
                 ids_list_redis = r_server.get("search_results:%s" % search_query)
                 ids_list = cPickle.loads(ids_list_redis)
@@ -56,7 +58,7 @@ class search(object):
                 print "set cache:search_results"
                 ids_list = sk.sku_info(id_select, search_query)
                 ids_list_for_cache = cPickle.dumps(ids_list)
-                r_server.set("search_results:%s" % search_query, ids_list_for_cache)
+                r_server.set("search_results:%s" % search_set_redis, ids_list_for_cache)
 
         r_server.expire("search_results:%s" % search_query, cache_timeout) 
         return render('search_results.mako', rp=ids_list, search_term=search_query)
